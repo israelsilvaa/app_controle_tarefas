da8edda0926998b6091f0066df948ab4f939664d
israel silva <Israel524.is@gmail.com>
2023-02-17 20:12:44 -0300

    aula 217
    Publicando e customizando o template de e-mail

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::resource('tarefa', 'App\Http\Controllers\TarefaController');
 
 Route::get('/mensagem-teste', function(){
-    // return new MensagemTesteMail();
-    Mail::to('israel524.is@gmail.com')->send(new MensagemTesteMail());
-    return 'E-mail enviado com secesso!';
+    return new MensagemTesteMail();
+    // Mail::to('israel524.is@gmail.com')->send(new MensagemTesteMail());
+    // return 'E-mail enviado com secesso!';
 });
 
