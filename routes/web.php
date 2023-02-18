85bfbde39e257fe3e16792e20652c9e24422a300
israel silva <Israel524.is@gmail.com>
2023-02-18 12:14:55 -0300

    aula 228
     Modificando a rota home da aplicação (redirectTo)

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', 'App\Http\Controllers\HomeController@index')
-    ->name('home')
-    ->middleware('verified');
+// Route::get('/home', 'App\Http\Controllers\HomeController@index')
+//     ->name('home')
+//     ->middleware('verified');
 
 Route::resource('tarefa', 'App\Http\Controllers\TarefaController')
     ->middleware('verified');
