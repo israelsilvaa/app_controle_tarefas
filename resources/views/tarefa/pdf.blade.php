4045ee03acebe67474c37e972ce7478323632fb9
israel silva <Israel524.is@gmail.com>
2023-02-19 15:52:21 -0300

    aula 254
    Adicionando suporte UTF-8, estilos CSS e quebras de páginas nas Views de PDF

--- resources/views/tarefa/pdf.blade.php
+++ resources/views/tarefa/pdf.blade.php
@@ -1,23 +1,58 @@
+<html lang="en">
 
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
+    <style>
+        .titulo{
+            border:1px;
+            background-color: #c2c2c2;
+            text-align: center;
+            whidth: 100%;
+            text-transform: uppercase;
+            font-weight: bold;
+            margin-bottom: 25px;
+        }
 
-<table>
-    <thead>
-        <tr>
-            <th>Id</th>
-            <th>Tarefa</th>
-            <th>data limite conclusão</th>
-        </tr>
-    </thead>
-    <tbody>
-        
-        @foreach($tarefas as $key => $tarefa)
+        .tabela{
+            width: 100%;
+        }
+
+        table th{
+            text-align: left;
+        }
+
+        .page-breack{
+            page-break-after: always; 
+        }
+
+    </style>
+</head>
+
+<body>
+    <div class="titulo">Lista de tarefas</div>
+    <table class="tabela">
+        <thead>
             <tr>
-                <td>{{$tarefa->id}}</td>
-                <td>{{$tarefa->tarefa}}</td>
-                <td>{{date('d/m/Y' ,strtotime($tarefa->data_limite_conclusao))}}</td>
+                <th>Id</th>
+                <th>Tarefa</th>
+                <th>data limite conclusão</th>
             </tr>
-        @endforeach
-        
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+
+            @foreach ($tarefas as $key => $tarefa)
+                <tr>
+                    <td>{{ $tarefa->id }}</td>
+                    <td>{{ $tarefa->tarefa }}</td>
+                    <td>{{ date('d/m/Y', strtotime($tarefa->data_limite_conclusao)) }}</td>
+                </tr>
+            @endforeach
+
+        </tbody>
+    </table>
+    <div class="page-breack"></div>
+    <h2>pagina 2</h2>
+</body>
+
+</html>
