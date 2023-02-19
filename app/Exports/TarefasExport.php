176157442e82fb9b0752a104caefe656f623fe9c
israel silva <Israel524.is@gmail.com>
2023-02-19 14:41:17 -0300

    aula 241
     Manipulando os dados exportados linha por linha e formatando datas

--- app/Exports/TarefasExport.php
+++ app/Exports/TarefasExport.php
@@ -5,9 +5,10 @@ namespace App\Exports;
 use App\Models\Tarefa;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 use Prophecy\Doubler\Generator\Node\ReturnTypeNode;
 
-class TarefasExport implements FromCollection, WithHeadings
+class TarefasExport implements FromCollection, WithHeadings, WithMapping
 {
     /**
      * @return \Illuminate\Support\Collection
@@ -20,11 +21,18 @@ class TarefasExport implements FromCollection, WithHeadings
     public function headings():array{
         return [
             'ID da Tarefa',
-            'ID do Usuário',
             'Tarefa',
-            'Data Limite coclusão',
-            'data criação',
-            'Data Atualização'];
+            'Data Limite coclusão'
+        ];
+    }
+
+    public function map($linha): array
+    {
+        return [
+            $linha->id,
+            $linha->tarefa,
+            date('d/m/Y', strtotime($linha->data_limite_conclusao))
+        ];
     }
 
 }
