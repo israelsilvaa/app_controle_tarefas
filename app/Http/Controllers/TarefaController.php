fd9ef4c6c50c362e2bc888b64457776b7e6a357b
israel silva <Israel524.is@gmail.com>
2023-02-19 15:56:18 -0300

    aula 256
    Laravel DOMPDF - Download vs Stream

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -128,8 +128,9 @@ class TarefaController extends Controller
 
     public function exportar() {
         $tarefas = Tarefa::where('user_id', '=', auth()->user()->id)->get();
-        
+
         $pdf = PDF::loadView('tarefa.pdf', ['tarefas'=> $tarefas]);
-        return $pdf->download('lista_de_tarefas.pdf');
+        // return $pdf->download('lista_de_tarefas.pdf');
+        return $pdf->stream('lista_de_tarefas.pdf');
     }
 }
